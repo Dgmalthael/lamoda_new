967872e567f43b4899cf349dc3a7109311c4884b
Deniz <85357861+Dgmalthael@users.noreply.github.com>
2022-04-22 13:36:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,14 +3,20 @@ from urllib.request import urlopen as req
 from datetime import datetime
 import pandas as pd
 
+LAST_PAGE_NUMBER = 2
+BASE_URL = "https://www.lamoda.ru/c/4152/default-men/?page="
+
+
+
 timestamp = datetime.now().strftime("%Y-%m-%d")
 brand = []
-pri = []
+prices = []
 type = []
 lns = []
-discount = []
-for a in range(1, 16):
-    my_url = "https://www.lamoda.ru/c/4152/default-men/?page=" + str(a)
+
+
+for a in range(1, LAST_PAGE_NUMBER):
+    my_url = BASE_URL + str(a)
     page = req(my_url)
     page_html = page.read()
     page.close()
@@ -18,45 +24,77 @@ for a in range(1, 16):
     # html prasing
     page_soup = bsoup(page_html, "html.parser")
 
-    # grab the info
-    products = page_soup.findAll('div', class_="products-catalog__list")
+# grab the info
+    products = page_soup.findAll('div', class_="x-product-card__card")
 
     for data in products:
         links = data.findAll('a', href=True)
+        #print(links)
+        for link in links:
+            y = link['href']
+            #print(link)
 
-    for link in links:
-        y = link['href']
-        # print(link)
-        lns.append(y)
-        # print(link)
-        dt = link.findAll('div', class_="to-favorites js-to-favorites to-favorites_wish-groups")
-        # print(dt)
+            lns.append(y)
+            #print(lns)
+            dt = link.findAll('div', class_="x-product-card-description")
+            # print(dt)
 
-        for data in dt:
-            x = data['data-brand']
-            brand.append(x)
-            x = data['data-name']
-            type.append(x)
-            if data.has_key("data-discount") == True:
-                x = data['data-price']
-                pri.append(x)
-                y = data['data-discount-percent']
-                discount.append(y)
-            if data.has_key("data-discount") == False:
-                x = data['data-price-origin']
-                pri.append(x)
-                discount.append(0)
+            for data in dt:
 
-fieldnames = ['Brand', 'Type', 'Price', 'Currency', 'Discount', 'Link']
+                br = data.findAll('div', class_="x-product-card-description__brand-name")
+                brtype = data.findAll('div', class_='x-product-card-description__product-name')
+                for datab in br:
+                    brand_name = datab.text
+                    brand.append(brand_name)
+                    #print(brand)
+                for datat in brtype:
+                    type_name = datat.text
+                    type.append(type_name)
+                    #print(type)
+
+                search = []
+                search_f = []
+
+                discounts = data.findAll('span',
+                                        class_="x-product-card-description__price-new x-product-card-description__price-WEB8507_price_no_bold")
+
+                orginals = data.findAll('span',
+                                        class_="x-product-card-description__price-single x-product-card-description__price-WEB8507_price_no_bold")
+                search.append(discounts)
+                search_f.append(orginals)
+                #print(orginals)
+                #print(search)
+                for i in range(len(search)):
+                    #print(search[i])
+                    if search[i] != "[]":
+                        for data_ds in discounts:
+                            discount = data_ds.text
+                            #print(discount)
+                            prices.append(discount)
+
+                    if search_f != "[]":
+                        for data_or in orginals:
+                            orginal = data_or.text
+                            prices.append(orginal)
+                            #print(orginal)
+
+#print(prices)
+fieldnames = ['Brand', 'Type', 'Price', 'Currency', 'Link']
 
 last = []
 
-for i in range(len(pri)):
-    a = {'Brand': brand[i], 'Type': type[i], 'Price': pri[i], 'Currency': 'RUB',
-         'Link': f'https://www.lamoda.ru/{lns[i]}/', 'Discount': discount[i]}
+for i in range(len(prices)):
+
+    a = {
+            'Brand': brand[i],
+            'Type': type[i],
+            'Price': prices[i],
+            'Currency': 'RUB',
+            'Link': f'https://www.lamoda.ru/{lns[i]}/'
+        }
     last.append(a)
 
 df = pd.DataFrame(last, columns=fieldnames)
-# df = df.drop('type', axis=1)
+
 print(df)
 df.to_csv(f'outputfile{timestamp}.csv')
